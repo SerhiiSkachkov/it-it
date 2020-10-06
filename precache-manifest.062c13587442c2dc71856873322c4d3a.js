a06fc40f9320fa7d470b4ec915225a4b41d275a2
SerhiiSkachkov <sergeyskachkov01@gmail.com>
2020-10-06 14:36:18 +0300

    Updates

---
+++ precache-manifest.062c13587442c2dc71856873322c4d3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49bca613179a99658ab0f1352e17c54",
+    "url": "/it-it/index.html"
+  },
+  {
+    "revision": "2430c8e091e81ccdd32a",
+    "url": "/it-it/static/css/main.4ddba63d.chunk.css"
+  },
+  {
+    "revision": "581e333654aefded7115",
+    "url": "/it-it/static/js/2.7147d112.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/it-it/static/js/2.7147d112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2430c8e091e81ccdd32a",
+    "url": "/it-it/static/js/main.efc7d974.chunk.js"
+  },
+  {
+    "revision": "825ca2ee3d1de6bcf2bb",
+    "url": "/it-it/static/js/runtime-main.1698e2a3.js"
+  },
+  {
+    "revision": "f7eab148df329432a159f8a1cccb102f",
+    "url": "/it-it/static/media/backdrop.f7eab148.jpg"
+  }
+]);
